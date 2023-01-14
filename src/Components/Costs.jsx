d4f5476c2db8ff8650ebca13058ca76257b315da
Atharv Phadnis <athiphadnis@gmail.com>
2023-01-15 00:09:54 +0530

    Included test scores statuses

--- src/Components/Costs.jsx
+++ src/Components/Costs.jsx
@@ -1,8 +1,8 @@
-import { useList } from 'react-firebase-hooks/database'
-import Table from 'react-bootstrap/Table'
-import { useContext } from 'react'
-import { ref } from 'firebase/database'
-import { FirebaseContext } from '../Contexts/FirebaseContext'
+import { useList } from "react-firebase-hooks/database"
+import Table from "react-bootstrap/Table"
+import { useContext } from "react"
+import { ref } from "firebase/database"
+import { FirebaseContext } from "../Contexts/FirebaseContext"
 
 export default function Costs() {
 	const { user, database } = useContext(FirebaseContext)
@@ -11,7 +11,7 @@ export default function Costs() {
 
 	const colorRowType = type => {
 		let style
-		switch(type) {
+		switch (type) {
 			case "Ambitious":
 				style = "danger"
 				break
@@ -27,6 +27,24 @@ export default function Costs() {
 		return `table-${style}`
 	}
 
+	const colorTestStatus = status => {
+		let style
+		switch (status) {
+			case "Score Sent":
+				style = "success"
+				break
+			case "Score Not Sent":
+				style = "danger"
+				break
+			case "Not Required":
+				style = "primary"
+				break
+			default:
+				style = ""
+		}
+		return `text-${style} fw-bold`
+	}
+
 	return (
 		<div className="table-responsive">
 			<Table bordered>
@@ -40,28 +58,47 @@ export default function Costs() {
 					</tr>
 				</thead>
 				<tbody>
-					{error && <tr><td colSpan={3}><strong>Error: {error}</strong></td></tr>}
-					{error && <tr><td colSpan={3}><strong>Loading...</strong></td></tr>}
-					{!loading && snapshots && (
+					{error && (
+						<tr>
+							<td colSpan={3}>
+								<strong>Error: {error}</strong>
+							</td>
+						</tr>
+					)}
+					{error && (
+						<tr>
+							<td colSpan={3}>
+								<strong>Loading...</strong>
+							</td>
+						</tr>
+					)}
+					{!loading &&
+						snapshots &&
 						snapshots.map(v => {
 							const data = v.val()
-							const total = data.fees + (data.gre ? 30 : 0) + (data.toefl ? 20 : 0)
+							const total =
+								data.fees +
+								(data.gre !== "Not Required" ? 30 : 0) +
+								(data.toefl !== "Not Required" ? 20 : 0)
 							grand_total += total
 							return (
 								<tr className={colorRowType(data.type)} key={v.key}>
 									<td>{data.university}</td>
-									<td>{data.gre ? "✔" : "❌"}</td>
-									<td>{data.toefl ? "✔" : "❌"}</td>
-									<td>$ {data.fees}</td>
-									<td>$ {total}</td>
+									<td className={colorTestStatus(data.gre)}>{data.gre}</td>
+									<td className={colorTestStatus(data.toefl)}>{data.toefl}</td>
+									<td className="text-end">$ {data.fees}</td>
+									<td className="text-end">$ {total}</td>
 								</tr>
 							)
-						})
+						})}
+					{!loading && snapshots && (
+						<tr className="table-dark">
+							<th className="text-center" colSpan={4}>
+								Grand Total
+							</th>
+							<th className="text-end">$ {grand_total}</th>
+						</tr>
 					)}
-					{!loading && snapshots && <tr className="table-dark">
-						<th className="text-center" colSpan={4}>Grand Total</th>
-						<th>$ {grand_total}</th>
-					</tr>}
 				</tbody>
 			</Table>
 		</div>
