41eae682958f6d4e329a5007bf3ca0f092a367a8
Atharv Phadnis <athiphadnis@gmail.com>
2023-01-15 00:04:27 +0530

    Removed unused "react-cookie" from code

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import { CookiesProvider } from 'react-cookie'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<CookiesProvider>
-			<App />
-		</CookiesProvider>
-	</React.StrictMode>,
+		<App />
+	</React.StrictMode>
 )
