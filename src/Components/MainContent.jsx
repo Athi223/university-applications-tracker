ff1a067a682b118a7c021964c89ff351d7aa73a9
Atharv Phadnis <athiphadnis@gmail.com>
2023-01-26 23:15:39 +0530

    Added Support for various Currencies

--- src/Components/MainContent.jsx
+++ src/Components/MainContent.jsx
@@ -1,14 +1,71 @@
 import { useContext, useRef, useState } from "react"
-import { FirebaseContext } from "../Contexts/FirebaseContext"
+import { FirebaseAuthContext } from "../Contexts/FirebaseAuthContext"
+import { FirebaseDBContext } from "../Contexts/FirebaseDBContext"
 import { Button, Card, Form, InputGroup, Modal } from "react-bootstrap"
-import Universities from "./Universities"
+import { useList } from "react-firebase-hooks/database"
 import { ref, child, push, update } from "firebase/database"
+import Universities from "./Universities"
 import Costs from "./Costs"
+import { useEffect } from "react"
+import currencies from "../currencies.json"
 
 export default function MainContent() {
-	const { user } = useContext(FirebaseContext)
+	const { user } = useContext(FirebaseAuthContext)
+	const { database, setUniversities, setCosts } = useContext(FirebaseDBContext)
 	const [showUniversityModal, setShowUniversityModal] = useState(false)
 	const [page, setPage] = useState(false)
+	const [currency, setCurrency] = useState("INR")
+	const [exchangeRates, setExchangeRates] = useState(null)
+	const [snapshots, loading, error] = useList(ref(database, `/users/${user.uid}/universities/`))
+
+	useEffect(() => {
+		fetch(`https://api.freecurrencyapi.com/v1/latest?apikey=${import.meta.env.VITE_FREECURRENCYAPI_KEY}`)
+			.then(response => response.json())
+			.then(data => setExchangeRates(data.data))
+	}, [])
+
+	useEffect(() => {
+		if (!loading && !error) {
+			const today = new Date()
+
+			setUniversities(
+				snapshots.map(v => {
+					const data = v.val()
+					const deadline = new Date(data.deadline)
+					const difference = Math.ceil((deadline - today) / (1000 * 60 * 60 * 24))
+					return {
+						key: v.key,
+						university: data.university,
+						deadline: deadline,
+						difference: difference,
+						type: data.type,
+						status: data.status,
+						sop: data.sop,
+						history: data.history,
+						lor1: data.lor1,
+						lor2: data.lor2,
+						lor3: data.lor3,
+						fees: data.fees,
+						feestatus: data.feestatus,
+						result: data.result,
+					}
+				})
+			)
+			setCosts(
+				snapshots.map(v => {
+					const data = v.val()
+					return {
+						key: v.key,
+						university: data.university,
+						fees: data.fees,
+						gre: data.gre,
+						toefl: data.toefl,
+						type: data.type,
+					}
+				})
+			)
+		}
+	}, [currency, error, loading])
 
 	return (
 		<>
@@ -25,14 +82,30 @@ export default function MainContent() {
 						<p>Costs</p>
 					</div>
 				</div>
-				<button className="btn btn-primary" onClick={() => setShowUniversityModal(true)}>
-					Add University
-				</button>
+				<div className="row">
+					<div className="col-auto">
+						<button className="btn btn-primary" onClick={() => setShowUniversityModal(true)}>
+							Add University
+						</button>
+					</div>
+					<div className="col">
+						<Form.Select
+							aria-label="Select Currency"
+							value={currency}
+							onChange={e => setCurrency(e.target.value)}>
+							{Object.keys(currencies).map(v => (
+								<option key={v} value={v}>
+									{v}
+								</option>
+							))}
+						</Form.Select>
+					</div>
+				</div>
 			</div>
 			<Card className="mt-3">
 				<Card.Body>
 					<Card.Title className="text-center text-info">{page ? "Costs" : "Applications"}</Card.Title>
-					{page ? <Costs /> : <Universities />}
+					{page ? <Costs currency={currency} exchangeRates={exchangeRates} /> : <Universities />}
 				</Card.Body>
 			</Card>
 			<AddUniversity showUniversityModal={showUniversityModal} setShowUniversityModal={setShowUniversityModal} />
@@ -41,7 +114,8 @@ export default function MainContent() {
 }
 
 function AddUniversity({ showUniversityModal, setShowUniversityModal }) {
-	const { user, database } = useContext(FirebaseContext)
+	const { user } = useContext(FirebaseAuthContext)
+	const { database } = useContext(FirebaseDBContext)
 	const closeUniversityModal = () => setShowUniversityModal(false)
 	const formRef = useRef()
 	const addUniversity = e => {
