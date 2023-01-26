87cf48adb5c7c6e6c5c8466f33fb08e0dde58d5c
Atharv Phadnis <athiphadnis@gmail.com>
2023-01-26 23:10:52 +0530

    Separate Unis and their Costs into 2 states

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap/dist/css/bootstrap.min.css"
+import { useState } from "react"
 import { initializeApp } from "firebase/app"
-import { getAuth } from 'firebase/auth'
-import { getDatabase } from 'firebase/database'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-import { useAuthState } from 'react-firebase-hooks/auth'
-import Authentication from './Components/Authentication'
-import MainContent from './Components/MainContent'
-import { FirebaseContext } from "./Contexts/FirebaseContext"
+import { getAuth } from "firebase/auth"
+import { getDatabase } from "firebase/database"
+import { useAuthState } from "react-firebase-hooks/auth"
+import Authentication from "./Components/Authentication"
+import MainContent from "./Components/MainContent"
+import { FirebaseAuthContext } from "./Contexts/FirebaseAuthContext"
+import { FirebaseDBContext } from "./Contexts/FirebaseDBContext"
 
 // Your web app's Firebase configuration
-import firebaseConfig from './firebaseConfig.json'
+import firebaseConfig from "./firebaseConfig.json"
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
@@ -19,19 +19,31 @@ const database = getDatabase(app)
 
 export default function App() {
 	const [user, loading, error] = useAuthState(auth)
+	const [universities, setUniversities] = useState([])
+	const [costs, setCosts] = useState([])
+
 	return (
-		<FirebaseContext.Provider value={{ auth, database, user }}>
+		<FirebaseAuthContext.Provider value={{ auth, user }}>
 			<div className="d-flex flex-column vh-100 vw-100">
 				<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
 					<div className="container-fluid">
-						<a className="navbar-brand" href="#">University Applications Tracker</a>
+						<a className="navbar-brand" href="#">
+							University Applications Tracker
+						</a>
 						<Authentication />
 					</div>
 				</nav>
 				<main className="flex-grow-1 p-4">
-					{user ? <MainContent /> : <h3 className="text-center">You need to be Signed In to access all features</h3>}
+					{user ? (
+						<FirebaseDBContext.Provider
+							value={{ database, universities, setUniversities, costs, setCosts }}>
+							<MainContent />
+						</FirebaseDBContext.Provider>
+					) : (
+						<h3 className="text-center">You need to be Signed In to access all features</h3>
+					)}
 				</main>
 			</div>
-		</FirebaseContext.Provider>
+		</FirebaseAuthContext.Provider>
 	)
 }
