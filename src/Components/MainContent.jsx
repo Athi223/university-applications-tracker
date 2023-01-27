c1eba48834d9ee5c9c785d765a0c46b573ff5331
Atharv Phadnis <athiphadnis@gmail.com>
2023-01-27 11:48:45 +0530

    Included new AddUniversity component

--- src/Components/MainContent.jsx
+++ src/Components/MainContent.jsx
@@ -1,11 +1,12 @@
-import { useContext, useRef, useState, useEffect } from "react"
+import { useContext, useState, useEffect } from "react"
 import { FirebaseAuthContext } from "../Contexts/FirebaseAuthContext"
 import { FirebaseDBContext } from "../Contexts/FirebaseDBContext"
 import { useList } from "react-firebase-hooks/database"
-import { ref, child, push, update } from "firebase/database"
-import { Button, Card, Form, InputGroup, Modal } from "react-bootstrap"
+import { ref } from "firebase/database"
+import { Card, Form } from "react-bootstrap"
 import Universities from "./Universities"
 import Costs from "./Costs"
+import AddUniversity from "./AddUniversity"
 import currencies from "../currencies.json"
 
 export default function MainContent() {
@@ -109,142 +110,5 @@ export default function MainContent() {
 			</Card>
 			<AddUniversity showUniversityModal={showUniversityModal} setShowUniversityModal={setShowUniversityModal} />
 		</>
-	)
-}
-
-function AddUniversity({ showUniversityModal, setShowUniversityModal }) {
-	const { user } = useContext(FirebaseAuthContext)
-	const { database } = useContext(FirebaseDBContext)
-	const closeUniversityModal = () => setShowUniversityModal(false)
-	const formRef = useRef()
-	const addUniversity = e => {
-		e.preventDefault()
-		const path = `/users/${user.uid}/universities/`
-		const URL = `${path}${push(child(ref(database), path)).key}`
-		const updates = {
-			[URL]: {
-				university: formRef.current.university.value,
-				type: formRef.current.type.value,
-				status: formRef.current.status.value,
-				deadline: formRef.current.deadline.value,
-				sop: formRef.current.sop.value,
-				lor1: formRef.current.lor1.value,
-				lor2: formRef.current.lor2.value,
-				lor3: formRef.current.lor3.value,
-				history: formRef.current.history.value,
-				fees: parseFloat(formRef.current.fees.value),
-				feestatus: formRef.current.feestatus.value,
-				gre: formRef.current.gre.value,
-				toefl: formRef.current.toefl.value,
-				result: "Pending",
-			},
-		}
-		formRef.current.reset()
-		return update(ref(database), updates)
-	}
-
-	return (
-		<Modal show={showUniversityModal} onHide={closeUniversityModal} backdrop="static" centered>
-			<Modal.Header closeButton>
-				<Modal.Title>Add University</Modal.Title>
-			</Modal.Header>
-			<Modal.Body>
-				<Form ref={formRef} onSubmit={addUniversity}>
-					<InputGroup className="mb-2">
-						<Form.Control type="text" placeholder="Enter University Name" id="university" required />
-						<Form.Select id="type" required>
-							<option value="Safe">Safe</option>
-							<option value="Moderate">Moderate</option>
-							<option value="Ambitious">Ambitious</option>
-						</Form.Select>
-					</InputGroup>
-					<InputGroup className="mb-2">
-						<InputGroup.Text>Application Status</InputGroup.Text>
-						<Form.Select id="status" required>
-							<option value="Not Applied">Not Applied</option>
-							<option value="In Process">In Process</option>
-							<option value="Applied">Applied</option>
-						</Form.Select>
-					</InputGroup>
-					<InputGroup className="mb-2">
-						<InputGroup.Text>Application Deadline</InputGroup.Text>
-						<Form.Control id="deadline" type="date" placeholder="Application Deadline" required />
-					</InputGroup>
-					<InputGroup className="mb-2">
-						<InputGroup.Text>Statement of Purpose</InputGroup.Text>
-						<Form.Select id="sop" required>
-							<option value="Not Submitted">Not Submitted</option>
-							<option value="Submitted">Submitted</option>
-							<option value="N/A">N/A</option>
-						</Form.Select>
-					</InputGroup>
-					<InputGroup className="mb-2">
-						<InputGroup.Text>Personal History Statement</InputGroup.Text>
-						<Form.Select id="history" required>
-							<option value="Not Submitted">Not Submitted</option>
-							<option value="Submitted">Submitted</option>
-							<option value="N/A">N/A</option>
-						</Form.Select>
-					</InputGroup>
-					<InputGroup className="mb-2">
-						<InputGroup.Text>Letter of Recommendation - 1</InputGroup.Text>
-						<Form.Select id="lor1" required>
-							<option value="Not Submitted">Not Submitted</option>
-							<option value="Submitted">Submitted</option>
-							<option value="N/A">N/A</option>
-						</Form.Select>
-					</InputGroup>
-					<InputGroup className="mb-2">
-						<InputGroup.Text>Letter of Recommendation - 2</InputGroup.Text>
-						<Form.Select id="lor2" required>
-							<option value="Not Submitted">Not Submitted</option>
-							<option value="Submitted">Submitted</option>
-							<option value="N/A">N/A</option>
-						</Form.Select>
-					</InputGroup>
-					<InputGroup className="mb-2">
-						<InputGroup.Text>Letter of Recommendation - 3</InputGroup.Text>
-						<Form.Select id="lor3" required>
-							<option value="Not Submitted">Not Submitted</option>
-							<option value="Submitted">Submitted</option>
-							<option value="N/A">N/A</option>
-						</Form.Select>
-					</InputGroup>
-					<InputGroup className="mb-2">
-						<Form.Control
-							type="number"
-							min={0}
-							max={1000}
-							placeholder="Application Fees in $"
-							id="fees"
-							required
-						/>
-						<Form.Select id="feestatus" required>
-							<option value="Not Paid">Not Paid</option>
-							<option value="Paid">Paid</option>
-						</Form.Select>
-					</InputGroup>
-					<InputGroup className="mb-2">
-						<InputGroup.Text>GRE:</InputGroup.Text>
-						<Form.Select id="gre" required>
-							<option value="Not Required">Not Required</option>
-							<option value="Score Not Sent">Score Not Sent</option>
-							<option value="Score Sent">Score Sent</option>
-						</Form.Select>
-						<InputGroup.Text>TOEFL:</InputGroup.Text>
-						<Form.Select id="toefl" required>
-							<option value="Not Required">Not Required</option>
-							<option value="Score Not Sent">Score Not Sent</option>
-							<option value="Score Sent">Score Sent</option>
-						</Form.Select>
-					</InputGroup>
-					<div className="text-center">
-						<Button variant="primary" type="submit">
-							Add
-						</Button>
-					</div>
-				</Form>
-			</Modal.Body>
-		</Modal>
 	)
 }
